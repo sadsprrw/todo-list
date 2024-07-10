f9c1b34371da9c045b76dc746a99400c101c5709
Eugen Vorobiov <jeka.vorobjov@gmail.com>
2024-07-10 15:33:25 +0300

    fix(core): add router view, initial api calls

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
-  <div></div>
+  <div>
+    <RouterView :key="$route.path" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
+import { useStore } from 'vuex'
 
 export default defineComponent({
   name: 'App',
+  setup() {
+    const store = useStore();
 
+    onMounted(() => {
+      const staredTodoIds = localStorage.getItem('staredTodoIds');
+      if (staredTodoIds) {
+        try {
+          const parsedList = JSON.parse(staredTodoIds);
+          store.commit('setStaredTodoIds', parsedList);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+      store.dispatch('getUsersList');
+    });
+  }
 });
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #545454;
 }
 </style>
