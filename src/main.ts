478c7b834023c3d2de6691db8692f1c52d1c09ea
Eugen Vorobiov <jeka.vorobjov@gmail.com>
2024-07-10 15:30:38 +0300

    fix(core): main file changes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from "./store";
+import router from "./router";
 
-createApp(App).mount('#app')
+import '@/assets/scss/main.scss';
+
+const app = createApp(App);
+app.use(store)
+app.use(router)
+app.mount('#app');
